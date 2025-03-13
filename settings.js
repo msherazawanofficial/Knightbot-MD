0e12e517727fd86018b8b7eaee92b3b415211229
msherazawanofficial <msherazawanofficial@gmail.com>
2025-03-13 03:23:36 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "M͡ R͡ _ֆɦɛʀǟʐ",
+  botOwner: 'M͡ R͡ _ֆɦɛʀǟʐ', // Your name
+  ownerNumber: '923138619519', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
